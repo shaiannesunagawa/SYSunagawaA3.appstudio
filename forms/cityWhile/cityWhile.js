fb66291ea5a4434c9d58c3c9851ff28983488edb
shaiannesunagawa <sys74530@creighton.edu>
2021-03-13 23:08:22 -0600

    Create cityWhile.js

---
+++ forms/cityWhile/cityWhile.js
@@ -0,0 +1,12 @@
+// Use two while loops. 
+// Get as many cities from the user as they want - stop getting cities when the user says to stop. 
+// The user can use any case mixture in their input (eg. SAN FranciSCO)
+// Cities go into an array named 'cities'.  
+// When city input is done, use a while loop that outputs to the console each city on its' own line in all lower case.
+/* The output must be in this format:
+
+eg. say the user inputs "OmaHa" and "St. LOUIS"
+The output would look like this:
+
+omaha
+st. louis */
