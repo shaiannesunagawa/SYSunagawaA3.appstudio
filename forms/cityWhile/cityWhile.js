4f5a37ca3ffd5c69654bfd544e48d8fa2f74dc7b
shaiannesunagawa <sys74530@creighton.edu>
2021-03-14 20:10:50 -0500

    working code

--- forms/cityWhile/cityWhile.js
+++ forms/cityWhile/cityWhile.js
@@ -1,24 +1,16 @@
-// Use two while loops. 
-// Get as many cities from the user as they want - stop getting cities when the user says to stop. 
-// The user can use any case mixture in their input (eg. SAN FranciSCO)
-// Cities go into an array named 'cities'.  
-// When city input is done, use a while loop that outputs to the console each city on its' own line in all lower case.
-/* The output must be in this format:
-eg. say the user inputs "OmaHa" and "St. LOUIS"
-The output would look like this:
-omaha
-st. louis */
-
 let cities = []
+
 let i = 0
+
 let doneWithCities = false
+
 while (doneWithCities == false) {
-    cities[i] = prompt("Enter the next city: ")
-    i++
+    let city = prompt("Enter the next city: ")
+    cities.push(city.toLowerCase())
     doneWithCities = confirm("Are you done with cities?")
     }
-console.log(cities)
-/*while (i < cities.length) {
-    let userCites = cities.toString()
-    console.log(userCities.toLowerCase)
-    }*/
+    
+while (i < cities.length){
+  console.log(cities[i])
+  i++
+}
